ecd29d385a6c008b30cde64e21c18fbf98b4f327
yohan <yohan@adelios.fr>
2024-11-27 12:14:59 +0100

    Loading: setup rotate animation

--- app/(loading)/page.tsx
+++ app/(loading)/page.tsx
@@ -1,17 +1,45 @@
+'use client';
 import Image from 'next/image';
+import React from 'react';
 
 export default function Loading() {
+  const wrapperImage = React.useRef<HTMLDivElement | null>(null);
+  let clientX;
+  let clientY;
+  React.useEffect(() => {
+    window.addEventListener('mousemove', (e) => {
+      clientX = e.clientX;
+      clientY = e.clientY;
+      const arrayOfImages: HTMLElement[] = Array.from(
+        wrapperImage.current!.children,
+      ) as HTMLElement[];
+      const normalizeSize = e.clientX / window.innerWidth - 0.5;
+      arrayOfImages[2].style.left = `${normalizeSize * 50}px`;
+      arrayOfImages[2].style.transform = `rotate(${normalizeSize * 5}deg)`;
+
+      arrayOfImages[1].style.left = `${normalizeSize * 200}px`;
+      arrayOfImages[1].style.transform = `rotate(${normalizeSize * 25}deg)`;
+
+      arrayOfImages[0].style.left = `${normalizeSize * 300}px`;
+      arrayOfImages[0].style.transform = `rotate(${normalizeSize * 50}deg)`;
+      // arrayOfImages[2].style.top = `${normalizeSize * 10}px`;
+    });
+  }, [clientX, clientY]);
+
   return (
     <div className="flex justify-center items-center relative w-full h-[100vh]">
-      <div className="border-2 border-orange-500  w-[35vw] h-[90vh] rounded-xl relative overflow-hidden">
-        {['desktop.jpg', 'tesla.jpg', 'holidays.jpg'].map((source, index) => (
+      <div
+        className="  w-[35vw] h-[90vh] rounded-xl relative"
+        ref={wrapperImage}
+      >
+        {['desktop.jpg', 'tesla.jpeg', 'holidays.jpg'].map((source, index) => (
           <Image
             key={index}
             src={`/images/${source}`}
             alt={source}
             width={0}
             height={0}
-            className="border-2 border-red-500 w-full h-full object-cover"
+            className="border-2  w-full h-full object-cover absolute rounded-3xl"
           />
         ))}
       </div>
