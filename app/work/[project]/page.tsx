c228c33fc34c45933150fd15cdfa9f5be1911d4c
yohan <yohan@adelios.fr>
2025-03-13 07:24:17 +0100

    feat: add hard coded image in the dynamic work route

--- app/work/[project]/page.tsx
+++ app/work/[project]/page.tsx
@@ -1,11 +1,24 @@
 'use client';
 import { useParams } from 'next/navigation';
 import React from 'react';
+import Image from 'next/image';
 
 const Work = () => {
   const { project } = useParams();
   console.log('project', project);
-  return <div>Work</div>;
+  return (
+    <div>
+      <div className="w-screen h-screen">
+        <Image
+          src={'/images/first-image.png'}
+          alt={'first-image'}
+          width={1000}
+          height={1000}
+          className="w-full h-full"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Work;
