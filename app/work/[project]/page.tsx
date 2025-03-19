d906de6bd5d053fc2ef5385f47584c375ddf1bca
yohan <yohan@adelios.fr>
2025-03-19 06:49:48 +0100

    feat: finish page work by title

--- app/work/[project]/page.tsx
+++ app/work/[project]/page.tsx
@@ -1,51 +1,25 @@
 import React from 'react';
-import Image from 'next/image';
 import { ProjectViewModel } from '@/src/viewmodels/ProjectViewModel';
-import Content from '@/common/components/Content/Content';
+import Project from '@/common/components/Project/Project';
 
 const projectViewModel = new ProjectViewModel();
 
 const Work = async ({ params }: { params: { project: string } }) => {
   const { project } = await params;
   const data = await projectViewModel.getProjectByTitle(project as string);
+  const mediaUrls = [];
 
-  console.log('data', data);
-
-  // let mediaUrls = [];
-
-  // const regex = /<(img|video|source)[^>]+src="([^">]+)"/g;
-  // let matches;
-  // while ((matches = regex.exec(data!.content)) !== null) {
-  //   mediaUrls.push(matches[2]);
-  // }
+  const regex = /<(img|video|source)[^>]+src="([^">]+)"/g;
+  let matches;
+  while ((matches = regex.exec(data!.content)) !== null) {
+    mediaUrls.push(matches[2]);
+  }
 
   if (!data) return <p>chargement ...</p>;
 
   return (
     <div>
-      <div className="w-screen h-screen border-2 border-red-500">
-        <Image
-          src={data!.featuredImage.src}
-          alt={'first-image'}
-          width={1000}
-          height={1000}
-          className="w-full h-full"
-        />
-
-        {/* {mediaUrls.map((element, index) => (
-          <Image
-            key={index}
-            src={element}
-            alt={'first-image'}
-            width={1000}
-            height={1000}
-            className="w-full h-full"
-          />
-        ))} */}
-        {/* <Content mediaUrls={mediaUrls} /> */}
-
-        <p>Test</p>
-      </div>
+      <Project data={data} mediaUrls={mediaUrls} />
     </div>
   );
 };
---
+++ common/components/Project/Project.tsx
@@ -0,0 +1,63 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { Project as ProjectType } from '@/src/models/Project';
+
+interface Props {
+  data: ProjectType;
+  mediaUrls: string[];
+}
+
+const Project = ({ data, mediaUrls }: Props) => {
+  document.body.style.overflow = 'visible';
+  document.body.style.height = 'auto';
+  return (
+    <div className="w-screen h-screen">
+      <div className="w-screen h-screen relative flex justify-center items-center">
+        <Image
+          src={data!.featuredImage.src}
+          alt={'first-image'}
+          width={1000}
+          height={1000}
+          className="w-full h-full absolute top-0 left-0"
+        />
+        <h1 className="relative z-1" style={{ fontSize: '5vw' }}>
+          {data.title}
+        </h1>
+      </div>
+
+      {mediaUrls.map((element, index) => {
+        if (element.endsWith('mp4')) {
+          return (
+            <div key={index} className="w-full h-full overflow-hidden">
+              <video
+                key={index}
+                loop
+                autoPlay
+                muted
+                playsInline
+                width={'100%'}
+                height={'100%'}
+              >
+                <source src={element} type="video/mp4" />
+              </video>
+            </div>
+          );
+        } else {
+          return (
+            <Image
+              key={index}
+              src={element}
+              alt={'first-image'}
+              width={1000}
+              height={1000}
+              className="w-full h-full relative z-20"
+            />
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+export default Project;
