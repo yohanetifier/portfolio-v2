6f7b102c6b337be7a7198a6a75c25fbd0d616e26
yohan <yohan@adelios.fr>
2025-04-23 19:41:36 +0200

    feat: add contact page component with email link

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center z-20 relative flex-col">
+      <h2 className="uppercase">Send me an email:</h2>
+      <a href="mailto:yohan.etifier@gmail.com">yohan.etifier@gmail.com</a>
+    </div>
+  );
+};
+
+export default page;
