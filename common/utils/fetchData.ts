4eaaf3c32564500ddb86735f666c388907926802
yohan <yohan@adelios.fr>
2025-04-23 20:41:12 +0200

    chore: remove unused import statement for API_URL in fetchData utility

--- common/utils/fetchData.ts
+++ common/utils/fetchData.ts
@@ -1,13 +1,16 @@
-import { API_URL } from '@/config/constants';
+// import { API_URL } from '@/config/constants';
 
 export const fetchData = async (query: string, variables = {}) => {
-  const res = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+  const res = await fetch(
+    'https://ivory-bat-745340.hostingersite.com/graphql',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query, variables }),
     },
-    body: JSON.stringify({ query, variables }),
-  });
+  );
   if (!res.ok) {
     throw new Error('Failed to fetch posts');
   }
