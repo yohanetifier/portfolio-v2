f60c4fe2e35fc09509560440d76a86414b202eae
Yohan <etifier.y@gmail.com>
2025-05-05 20:56:48 +0200

    refactor: comment out Fullscreen component code for future implementation

--- common/components/Fullscreen/Fullscreen.tsx
+++ common/components/Fullscreen/Fullscreen.tsx
@@ -1,17 +1,17 @@
-'use client';
-import { useFullscreenContext } from '@/contexts/FullscreenContext';
-import React from 'react';
+// 'use client';
+// import { useFullscreenContext } from '@/contexts/FullscreenContext';
+// import React from 'react';
 
-const Fullscreen = () => {
-  const { isFullscreenVisible } = useFullscreenContext();
-  return (
-    <>
-      <div
-        className="fixed top-0 w-screen h-screen z-[2] border-2 border-red-500"
-        id="fullscreen"
-      ></div>
-    </>
-  );
-};
+// const Fullscreen = () => {
+//   const { isFullscreenVisible } = useFullscreenContext();
+//   return (
+//     <>
+//       <div
+//         className="fixed top-0 w-screen h-screen z-[2] border-2 border-red-500"
+//         id="fullscreen"
+//       ></div>
+//     </>
+//   );
+// };
 
-export default Fullscreen;
+// export default Fullscreen;
